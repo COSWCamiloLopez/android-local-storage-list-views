03d78b638ef8c826b08225e7f1fdb6b8924e07ee
Camilo López <camilino123@gmail.com>
2019-05-01 17:25:09 -0500

    SharedPreference instance changed, token and file string obtained from strings.xml file

--- app/src/main/java/com/eci/cosw/taskplanner/Activity/LaunchActivity.java
+++ app/src/main/java/com/eci/cosw/taskplanner/Activity/LaunchActivity.java
@@ -6,23 +6,29 @@ import android.os.Bundle;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.eci.cosw.taskplanner.R;
 import com.eci.cosw.taskplanner.Util.SharedPreference;
 
 public class LaunchActivity extends AppCompatActivity {
 
-    private static final String TOKEN_KEY = "TOKEN_KEY";
     private SharedPreference sharedPreference;
+    private String TOKEN_KEY;
+    private String file;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        sharedPreference = new SharedPreference(this);
 
-        if (sharedPreference.containsToken()) {
-            Intent mainIntent = new Intent(this, MainActivity.class);
+        file = getString(R.string.preference_file_key);
+        TOKEN_KEY = getString(R.string.token_key);
+
+        sharedPreference = new SharedPreference(LaunchActivity.this, file);
+
+        if (sharedPreference.contains(TOKEN_KEY)) {
+            Intent mainIntent = new Intent(LaunchActivity.this, MainActivity.class);
             startActivity(mainIntent);
         } else {
-            Intent loginIntent = new Intent(this, LoginActivity.class);
+            Intent loginIntent = new Intent(LaunchActivity.this, LoginActivity.class);
             startActivity(loginIntent);
         }
     }
