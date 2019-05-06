653ec19753306b5c279161baef4df808a4b35f9b
Camilo López <camilino123@gmail.com>
2019-05-05 19:15:49 -0500

    Data type converter added and taskDao instance created

--- app/src/main/java/com/eci/cosw/taskplanner/db/AppDatabase.java
+++ app/src/main/java/com/eci/cosw/taskplanner/db/AppDatabase.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
+import androidx.room.TypeConverters;
 
 import com.eci.cosw.taskplanner.Model.Task;
 import com.eci.cosw.taskplanner.Model.User;
@@ -13,9 +14,12 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 @Database(entities = {User.class, Task.class}, version = 1)
+@TypeConverters({DateConverter.class})
 public abstract class AppDatabase extends RoomDatabase {
 
     private static AppDatabase INSTANCE;
+
+    public abstract TaskDao taskModel();
 
     private static final ExecutorService executorService = Executors
             .newFixedThreadPool(1);
